8e754ae44a80bb7fa8e7b6b90ba89e104f29599e
LeeScofield <175525316@qq.com>
2018-08-07 23:31:04 +0800

    lesson 10

--- javascript/vue2.js
+++ javascript/vue2.js
@@ -6,7 +6,41 @@ new Vue({
 	},
 	methods:{
 		printName:function(){
-			this.name = this.$aaa.name.value;
+			this.name = this.$refs.name.value;
+		}
+	}
+});
+
+new Vue({
+	el:"#vue-el1",
+	data:{
+		a:0,
+		b:0,
+		c:0,
+		d:0,
+		age:10
+	},
+	methods:{
+		addA:function(){
+			console.log("methed A");
+			return this.a;
+		},
+		addB:function(){
+			console.log("method B");
+			return this.age + this.b;
+		},
+		addTest:function(){
+			console.log("method Test");
+			this.c = this.c + 1;
+		},
+		addTest2:function(){
+			console.log("method Test2");
+			this.d++;
+		}
+	},computed:{
+		now:function(){
+			console.log("method now")
+			return Date.now();
 		}
 	}
 });

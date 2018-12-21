4b67ec38aaeb93e6217ec717a87c200704f06fc0
Lee <175525316@qq.com>
2018-12-21 16:39:38 +0800

    数组合并

--- javascript/vue1.js
+++ javascript/vue1.js
@@ -3,13 +3,13 @@
 new Vue({
 	el:"#vue-el",  //document元素
 	data:{
-		name:"颜良，文丑", //document数据,
-		age:45,
-		sex:'男',
-		webSite:'http://www.baidu.com',
-		siteName:'百度',
-		webSiteTag:'<a href="http://www.hao123.com">hao123</a>'
-	},
+        name:"颜良，文丑", //document数据,
+        age:45,
+        sex:'男',
+        webSite:'http://www.baidu.com',
+        siteName:'百度',
+        webSiteTag:'<a href="http://www.hao123.com">hao123</a>'
+    },
 	methods:{
 		greet:function(name){
 			return "欢迎："+name+" and " + this.name;
